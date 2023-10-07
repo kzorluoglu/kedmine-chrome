c63fec4f4c54942e7d1f132e95329bc9f7480842
Koray Zorluoglu <koray@d8devs.com>
2023-10-07 19:03:16 +0200

    ref #timer,timentry,searchentry: components created

--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 const app = createApp(App)
 
+
+// Define a global property
+app.config.globalProperties.createNewTimer = function() {
+  // This is just a dummy console log to see if the function is invoked
+  console.log("New timer should be created here!");
+
+  // Logic to communicate with your App.vue or any other component
+  // to actually create a timer or set properties
+};
+
 app.use(router) // This is how you inject the router into the Vue app
 
 // Mount the app to the DOM
